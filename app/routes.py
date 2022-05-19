0c961ea90ca495f296b8578de625229d9702a64d
Brandt Carlson <brandt.carlson11@gmail.com>
2022-05-19 10:24:48 -0500

    building api funcs

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,17 @@
 from app import app
 from flask import render_template
 import requests as r
+from random import randrange
+from app.services import add_char
+
 
 @app.route('/')
 def home():
+    
     return render_template('index.html')
+
+@app.route('/rm')
+def rm():
+    x = randrange(1, 826)
+    add_char(x)
+    return render_template('rm.html')
--- app/services.py
+++ app/services.py
@@ -1,2 +1,67 @@
 import requests as r
+from random import randrange
 
+
+class Char:
+    def __init__(self, name, status, species, gender, origin, image, id):
+        self.name = name
+        self.status = status
+        self.species = species
+        self.gender = gender
+        self.origin = origin
+        self.image = image
+        self.id = id
+
+class CharDict:
+    def __init__(self):
+        self.chars = {}
+    
+    def add_1_char(self, id):
+        response = r.get('https://rickandmortyapi.com/api/character/' + str(id) + "/")
+        if response.status_code == 200:
+            data = response.json()
+        name = data['name']
+        status = data['status']
+        species = data['species']
+        gender = data['gender']
+        origin = data['origin']['name']
+        image = data['image']
+        id = data['id']
+        new_char = Char(name, status, species, gender, origin, image, id)
+        self.chars[id] = new_char    
+
+    def add_rand_char(self):
+        x = randrange(1, 826)
+        response = r.get('https://rickandmortyapi.com/api/character/' + str(x) + "/")
+        if response.status_code == 200:
+            data = response.json()
+        name = data['name']
+        status = data['status']
+        species = data['species']
+        gender = data['gender']
+        origin = data['origin']['name']
+        image = data['image']
+        id = data['id']
+        new_char = Char(name, status, species, gender, origin, image, id)
+        self.chars[id] = new_char 
+
+    def add_mult_char(self, arr):
+        response = r.get('https://rickandmortyapi.com/api/character/' + str(arr))
+        if response.status_code == 200:
+            data = response.json()
+        for x in range(len(arr)):
+            name = data[x]['name']
+            status = data[x]['status']
+            species = data[x]['species']
+            gender = data[x]['gender']
+            origin = data[x]['origin']['name']
+            image = data[x]['image']
+            id = data[x]['id']
+            new_char = Char(name, status, species, gender, origin, image, id)
+            self.chars[id] = new_char 
+    
+
+
+
+
+        
