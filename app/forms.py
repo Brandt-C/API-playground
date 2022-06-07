e27cf3b402e4eb5978e5379a5bbbb5436d971072
Brandt Carlson <brandt.carlson11@gmail.com>
2022-06-07 13:52:36 -0500

    tweaks

--- app/forms.py
+++ app/forms.py
@@ -15,4 +15,4 @@ class Locform(FlaskForm):
 class BioForm(FlaskForm):
     type_choice = RadioField("Techiness level", choices=["I'm NOT techy", "I'm Techy"], default="I'm NOT techy")
     l_choice = RadioField("Length", choices=['XS', 'TL;DR', 'Longer', 'Really Long'], default='XS')
-    submit = SubmitField('Change')
+    submit = SubmitField('Change Bio')
